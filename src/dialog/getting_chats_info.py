47fe32c283f8dd491571867252bf8e7ebb5ce705
BuildTools <unconfigured@null.spigotmc.org>
2023-10-03 20:09:36 +0400

    some more stuff

--- src/dialog/getting_chats_info.py
+++ src/dialog/getting_chats_info.py
@@ -9,5 +9,31 @@ def geting_chats_info(driver):
     #div type = INITIAL
     w1 = WebScraper(driver, "dict_otklik")
     # Find element by class ending with 'faTkYO'
-    if w1.is_it_on_the_page("chat_page"):
-        
+    chat_elements = w1.is_it_on_the_page("chat_element", only_one=False)
+    if chat_elements:
+        for chat_element in chat_elements:
+            #get id
+            chat_id = chat_element.get_attribute("id")
+            print(chat_id)
+            w2 = WebScraper(driver, "dict_otklik")
+            top_string = w2.is_it_on_the_page("top_string")
+            if top_string:
+                print(top_string.text)
+            galki = w2.is_it_on_the_page("galki")
+            if galki:
+                transform_style = galki.get_attribute("style")
+                if "scale(0);" in transform_style:
+                    print("SVG element  is hidden.")
+                else:
+                    print("SVG element  is visible.")
+                    svg = galki.find_element_by_tag_name("svg")
+                    paths = svg.find_elements(By.TAG_NAME, 'path')
+    
+                    for i, path in enumerate(paths):
+                        stroke_color = path.get_attribute('stroke')
+                        stroke_dashoffset = path.get_attribute('stroke-dashoffset')
+
+                        if stroke_color == "#0075ff" and stroke_dashoffset == "0":
+                            print("SVG element  is of the first type.")
+                        elif stroke_color == "#7e8287" and stroke_dashoffset == "10":
+                            print("SVG element  is of the second type.")
