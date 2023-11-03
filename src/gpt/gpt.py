970a7f744aa05cab7b3c371c9acc087eb4a3165b
BuildTools <unconfigured@null.spigotmc.org>
2023-11-03 21:55:59 +0400

    fast response
    added functions to gpt

--- src/gpt/gpt.py
+++ src/gpt/gpt.py
@@ -18,6 +18,8 @@ from random import randint
 from time import time
 
 from gpt.ask_gpt import write_answer_to_file
+from gpt.get_user_data import get_user_data
+from gpt.get_application import get_application
 
 def gpt(html_about:str, id: str, all_text_to_gpt_with_numbers:str, witch:int = 1, gpt4: bool = True,  temp: float = 2/10, timeout=240, sql = None):
     temp = randint(1,9)/10
@@ -29,25 +31,49 @@ def gpt(html_about:str, id: str, all_text_to_gpt_with_numbers:str, witch:int = 1
         client = client4
     else:
         client = client3
-    user_date = None
+    strings_dict = read_strings_from_file()
+    """user_date = None
     strings_dict = read_strings_from_file()
     report= strings_dict['report']
     try:
         get_userdate = f"''' {all_text_to_gpt_with_numbers} '''\n{report}"
         user_date, time_d = ask_gpt(client3, get_userdate, client3_temp, 210, id)
     except Exception as _:
-        user_date = html_about
-
+        user_date = html_about"""
+    start_time = time()
+    user_date = get_user_data(html_about)
+    user_date = user_date if user_date else html_about
+    
     middle_one = randint(1,3)
     
-    midle_text = strings_dict[f'midle_text_{middle_one}']
-    request1 = get_request1(witch, user_date, midle_text = midle_text)
-    answer1, time_dur = ask_gpt(client, request1, temp, timeout, id)
-    request2 = get_request2(witch, user_date, midle_text = f"{answer1} \n {midle_text}")
-    answer2, time_dur = ask_gpt(client, request2, temp, timeout, id)
-    start_time = time()
-    write_answer_to_file(start_time, f"{answer1}\n{midle_text} {answer2}", temp, time_dur, id, sql = sql)
-    return f"{answer1}\n{midle_text} {answer2}"
+    middle_text = strings_dict[f'midle_text_{middle_one}']
+    
+    
+
+    application = get_application(user_date, middle_text, whant_distant=bool(user_date.get("remote", False)), temperature=temp)
+    privetstvie = application.get("privetstvie", None)
+    distant_advertasing = application.get("distant_advertasing", None)
+    proshanie = application.get("proshanie", None)
+    naputstvie = application.get("naputstvie", None)
+    write_answer_to_file(start_time, f"""{privetstvie}
+{middle_text}
+{distant_advertasing}
+{proshanie}""",
+temp,
+time() - start_time,
+id,
+sql = sql)
+    return f"""{privetstvie}
+{middle_text}
+{distant_advertasing}
+{proshanie}""", naputstvie
+    # request1 = get_request1(witch, user_date, midle_text = midle_text)
+    # answer1, time_dur = ask_gpt(client, request1, temp, timeout, id)
+    # request2 = get_request2(witch, user_date, midle_text = f"{answer1} \n {midle_text}")
+    # answer2, time_dur = ask_gpt(client, request2, temp, timeout, id)
+    # start_time = time()
+    # write_answer_to_file(start_time, f"{answer1}\n{midle_text} {answer2}", temp, time_dur, id, sql = sql)
+    # return f"{answer1}\n{midle_text} {answer2}"
 
     #request = get_request(witch, user_date)
 
--- src/otklik/filling_the_card.py
+++ src/otklik/filling_the_card.py
@@ -42,17 +42,18 @@ def fill_text_field(id, texteria, text_gpt, driver):
 
 def process_card(id, w3, all_text, all_text_to_gpt_with_numbers, sql, driver):
     texteria = w3.is_it_on_the_page("textarea")
+    naputstvie = None
     if not texteria:
         logger.error(f"Не найден элемент textarea на карточке {id}")
-        return False
-
+        return False, naputstvie
+    
     try:
-        text_gpt = gpt(all_text, id, all_text_to_gpt_with_numbers, timeout=240, sql=sql)
+        text_gpt, naputstvie = gpt(all_text, id, all_text_to_gpt_with_numbers, timeout=240, sql=sql)
         if text_gpt is None:
             raise GPTException("Не удалось получить текст от GPT")
         
         if fill_text_field(id, texteria, text_gpt, driver):
-            return True
+            return True, naputstvie
 
     except (GPTException, Exception) as e:
         logger.warning(f"Не удалось получить текст от GPT на карточке {id}. Ошибка: {e}")
@@ -63,39 +64,39 @@ def process_card(id, w3, all_text, all_text_to_gpt_with_numbers, sql, driver):
         try:
             click(avtootklik, driver, buttomtype="otklik1")
             logger.info(f"Выбран автоотклик на карточке {id}")
-            return True
+            return True, naputstvie
         except Exception as e:
             logger.warning(f"Не удалось выбрать автоотклик на карточке {id}. Ошибка: {e}")
 
     logger.error(f"Не удалось обработать карточку {id}")
-    return False
+    return False, naputstvie
 
 
 def filling_the_card(driver: Safari, id: str, all_text:str, w3: WebScraper, all_text_to_gpt_with_numbers, sql) -> bool:    
-
+    naputstvie = None
     logger.info("Заполнение формы отклика на карточке %s", id)
     # textarea заполняется через js
     actions = ActionChains(driver)
-
-    if not process_card(id, w3, all_text, all_text_to_gpt_with_numbers, sql, driver):
-        return False
+    card_processed, naputstvie = process_card(id, w3, all_text, all_text_to_gpt_with_numbers, sql, driver)
+    if not card_processed:
+        return False, naputstvie
     
     sleep(0.5 + random())
     logger.info("Выбор цены на карточке %s", id)
     price_conteiner = w3.is_it_on_the_page("price_conteiner2")
     if not price_conteiner:
         logger.error("Не удалось найти контейнер цены на карточке %s", id)
-        return False
+        return False, naputstvie
     w4 = WebScraper(price_conteiner, "dict_otklik")
     price_input = w4.is_it_on_the_page("price_input")
     div_price_dur = w3.is_it_on_the_page("duration_chois_conteiner")
     
     if not price_input or not div_price_dur:
         logger.error("Не удалось найти поле цены %s или продолжительности %s на карточке %s", str(price_input), str(div_price_dur), id)
-        return False
+        return False, naputstvie
     if price_input.get_attribute("value") == "6000":
         logger.info("Цена уже заполнена на карточке %s", id)
-        return True
+        return True, naputstvie
     #scroll_down(driver, 1, 2000)
     driver.execute_script("arguments[0].scrollIntoView();", price_input)
     #actions.move_to_element(price_input).perform()
@@ -114,11 +115,11 @@ def filling_the_card(driver: Safari, id: str, all_text:str, w3: WebScraper, all_
     p_90 = w5.is_it_on_the_page("duration_90")
     if not p_90:
         logger.error("Не удалось найти продолжительность на карточке %s", id)
-        return False
+        return False, naputstvie
     driver.execute_script("arguments[0].focus();", p_90)
     sleep(0.1)
     
     actions.move_to_element(p_90).perform()
     p_90.click()
     logger.info("Все заполнено на карточке %s", id)    
-    return True
+    return True, naputstvie
